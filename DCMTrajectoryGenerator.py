231c61076e5ef0659d2651dcb5d32c8aa015ac3c
Michel-Abela <michel.abela@epfl.ch>
2024-10-06 12:36:07 +0200

    completed the python file with formulas

--- DCMTrajectoryGenerator.py
+++ DCMTrajectoryGenerator.py
@@ -27,15 +27,15 @@ class DCMTrajectoryGenerator:
 
     def getCoM(self):
         #This function generates the CoM trajectory by integration of CoM velocity(that has been found by the DCM values)
-        self.CoMDot= #todo: use equation (3) in the project description
-        self.CoM=  #todo: Simple euler numerical integration
+        self.CoMDot= self.omega * (self.DCM - self.CoM) #todo: use equation (3) in the project description
+        self.CoM= self.CoM + self.CoMDot * self.timeStep #todo: Simple euler numerical integration
         self.CoMDotPrev=self.CoMDot
         return self.CoM
 
     
     def planDCMTrajectory(self,time): #The output of this function is DCM posiiton
         #todo: use the equation 9 of the project description
-        self.DCM = 
+        self.DCM = (self.initialDCM - self.CoP) * math.exp(self.omega * time) + self.CoP
         pass
 
     
